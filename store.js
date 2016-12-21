1d40a264975d66b9502f941f34ece70c0a21aa12
Gerhard Preuss <gelipp@gmail.com>
2016-12-21 13:53:55 +0100

    move all jet logic to store.js

--- store.js
+++ store.js
@@ -1,14 +1,26 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux'
-import {sorted} from 'redux-jet'
+import {sorted, get, fetch} from 'redux-jet'
 import thunk from 'redux-thunk'
+import connection from './connection'
+
+const query = {
+  path: {startsWith: 'todo/#'},
+  sort: {byValueField: {id: 'number'}, from: 1, to: 100}
+}
 
 export default (initialState) => {
-  const initialTodos = initialState ? initialState.todos : []
-
   const store = createStore(combineReducers({
-    todos: sorted('todos', initialTodos),
+    todos: sorted('todos', initialState ? initialState.todos : []),
     filter: (state = 'all', action) => action.type === 'SET_FILTER' ? action.filter : state,
     setAllValue: (state = true, action) => action.type === 'TOGGLE_SET_ALL_VALUE' ? !state : state
   }), applyMiddleware(thunk))
+
+  if (initialState) {
+    store.resume = () => fetch(connection, query, 'todos')(store.dispatch)
+  } else {
+    store.getInitialState = () => {
+      return get(connection, query, 'todos')(store.dispatch).then(() => store.getState())
+    }
+  }
   return store
 }
